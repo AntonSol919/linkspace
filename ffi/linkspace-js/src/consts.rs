c770b738d1d3e29d4a4c9a081e581e6e17ec2e5c
Azon <Antonsol919@gmail.com>
2023-11-05 09:22:33 +0100

    move js Consts to static getter on CONSTS class

--- ffi/linkspace-js/src/consts.rs
+++ ffi/linkspace-js/src/consts.rs
@@ -2,17 +2,18 @@ use linkspace_pkt::consts::*;
 use wasm_bindgen::prelude::*;
 
 #[derive(Copy, Clone)]
-#[wasm_bindgen]
+#[wasm_bindgen(js_name="CONSTS")]
 pub struct LkConsts;
-#[wasm_bindgen]
-pub fn get_consts() -> LkConsts {
-    LkConsts
-}
 
-#[wasm_bindgen]
+#[wasm_bindgen(js_class="CONSTS")]
 impl LkConsts {
+
     #[wasm_bindgen(getter, js_name = "PUBLIC")]
-    pub fn public_self(&self) -> Box<[u8]> {
+    pub fn public_static() -> Box<[u8]> {
         PUBLIC.0.into()
+    }
+    #[wasm_bindgen(getter, js_name = "PRIVATE")]
+    pub fn private_static() -> Box<[u8]> {
+        PRIVATE.0.into()
     }
 }
